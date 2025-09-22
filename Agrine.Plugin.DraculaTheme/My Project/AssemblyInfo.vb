472ee4f6f371018e7b028bc9016d16e3a029b8ca
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-22 23:39:22 +0330

    - Add Agrine.Plugin.DraculaTheme Library To Solution

---
+++ Agrine.Plugin.DraculaTheme/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Agrine.Plugin.DraculaTheme")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Agrine.Plugin.DraculaTheme")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0b7a82f0-709e-4863-a773-e62b0e68e02d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
